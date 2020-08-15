f51b4bad43aee3e85b0d811bdef83040921cae95
PAN <yingyingpannbu@hotmail.com>
2020-08-15 17:30:17 -0400

    first commit for hw3q3

---
+++ hw3q3.sh
@@ -0,0 +1,7 @@
+#write a script to find all files in user directory start with 'readme'
+#!/ban/bash
+echo '----------script start----------'
+find ~/ -name 'readme*'
+echo '----------script shutdown----------'
+
+
