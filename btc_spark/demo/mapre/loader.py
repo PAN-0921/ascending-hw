ee189a536a4f0388bc799ed01361bb3114b64e40
PAN <yingyingpannbu@hotmail.com>
2020-10-06 23:12:56 -0400

    spark_hands_on

--- btc_spark/demo/mapre/loader.py
+++
@@ -1,8 +0,0 @@
-from .spark_config import sparkSession
-
-def loadCSVfrom(file_path:str):
-	df=sparkSession.read.csv(file_path,header=True,sep=",")
-	return df
-
-df=loadCSVfrom(file_path="/home/pan/Documents/tutorial/btc_spark/data/btc.csv")
-df.show();
--- btc_spark/demo/mapre/spark_config.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SparkSession
-
-conf=SparkConf().setAppName("frugalops").setMaster("local[*]")
-sc=SparkContext(conf=conf)
-
-sparkSession = SparkSession.builder.appName("frugalops").master("local[*]").getOrCreate()
